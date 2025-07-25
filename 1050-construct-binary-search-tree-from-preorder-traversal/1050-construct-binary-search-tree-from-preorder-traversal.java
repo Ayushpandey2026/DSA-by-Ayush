9fcb84320b950af7070dfa6cf4e43246856f7c2c
Ayush Pandey <ayushpandey02003@gmail.com>
2025-07-25 15:38:02 +0530

    Time: 2 ms (5.42%), Space: 42 MB (30.17%) - LeetHub

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,22 @@
+class Solution {
+     int preindex = 0;
+    Map<Integer,Integer> map = new HashMap<>();
+    private TreeNode build(int[] preorder, int left, int right){
+        if(left > right) return null;
+        int rootval = preorder[preindex++];
+        TreeNode root = new TreeNode(rootval);
+        int index = map.get(rootval);
+        root.left = build(preorder,left,index-1);
+        root.right = build(preorder,index+1,right);
+        return root;
+
+    }
+    public TreeNode bstFromPreorder(int[] preorder) {
+    int[] copy = Arrays.copyOf(preorder, preorder.length);
+    Arrays.sort(copy);
+        for(int i = 0; i< preorder.length;i++){
+             map.put(copy[i], i );
+        }
+        return build(preorder,0,preorder.length-1);
+    }
+}
