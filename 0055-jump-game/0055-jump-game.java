df4288b2ba4b4c8cebcc9b037b83fb9282871359
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-09 01:26:40 +0530

    Time: 3 ms (48.8%), Space: 45.8 MB (55.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,8 +5,7 @@ class Solution {
         for(int  i = 0;i< nums.length; i++){ 
             if( i > max) return false;
             else    max = Math.max(max,nums[i]+ i);
-
         }
         return true;
-    }
+    }    
 }
