6d5c136ec8362815df4bdb97de7ac3b1b1ca5824
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-13 17:04:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (7.85%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] a) {
+        int m =a.length;
+       for (int i = 0; i < m; i++) {
+            for (int j = 0; j <= i; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+
+            }
+        }
+         for (int i = 0; i < m; i++) {
+           int x=0,y=m-1;
+        while(x<y){
+            int temp = a[i][x];
+            a[i][x]= a[i][y];
+            a[i][y]= temp;
+            x++;
+            y--;
+        }
+         }
+    }
+}
