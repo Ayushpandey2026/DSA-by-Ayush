b23fe87cf61db40b90a2466b47694543d63bd8ab
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-21 07:23:44 +0530

    Time: 1 ms (91.81%), Space: 45.2 MB (35.65%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,26 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        Queue<TreeNode> que = new ArrayDeque<>();
-        que.add(root);
-
-        while (!que.isEmpty()) {
-            int size = que.size();  // number of nodes in current level
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = que.poll();
-                level.add(node.val);
-
-                if (node.left != null) que.add(node.left);
-                if (node.right != null) que.add(node.right);
-            }
-
-            ans.add(level);  // add the current level to answer
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null) return list;
+        Queue<TreeNode> q  = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+         int size = q.size();
+         List<Integer> al = new ArrayList<>();
+         for(int i=0;i<size;i++){
+           TreeNode node = q.poll();
+           al.add(node.val);
+           if(node.left!=null) q.add(node.left);
+           if(node.right!=null) q.add(node.right);
+         }
+           list.add(al);
         }
-
-        return ans;
+        return list;
     }
 }
