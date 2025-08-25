c84ba18c0c5b07cd395cd735adfeedab890332a0
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-25 18:21:02 +0530

    Time: 3 ms (76.13%), Space: 50.3 MB (67.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i =0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        int min = nums.length/3;
-        List<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue()>min){
-                list.add(entry.getKey());
+         int n = nums.length;
+       int candidate1 = 0, candidate2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+
+        for(int num : nums){
+            if(num == candidate1) cnt1++;
+            else if(num == candidate2) cnt2++;
+            else if(cnt1==0){
+                candidate1 = num;
+                cnt1 =1;
+            }
+            else if(cnt2==0){
+                candidate2 = num;
+                cnt2=1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1=0; cnt2=0;
+        for(int num : nums){
+            if(num == candidate1) cnt1++;
+            else if(num == candidate2) cnt2++;
+        }
+        List<Integer> list = new ArrayList<>();
+        if(cnt1 > n/3) list.add(candidate1);
+        if(cnt2 > n/3) list.add(candidate2);
         return list;
+
     }
 }
