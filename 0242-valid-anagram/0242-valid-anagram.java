86579422385ae87a37581736b1653bb7cd154eda
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-30 18:06:37 +0530

    Time: 8 ms (32.22%), Space: 43.2 MB (70.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(s2);
-        return Arrays.equals(s1,s2);
+       int[] freq = new int[26];
+       for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+       }
+
+       for(int val : freq){
+         if(val!=0) return false;
+       }
+       return true;
     }
 }
