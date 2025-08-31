3fa7a8eb37cfdfa11c4d734bf89ffd0186ec5ba8
Ayush Pandey <ayushpandey02003@gmail.com>
2025-08-31 12:58:04 +0530

    Time: 0 ms (100%), Space: 41.7 MB (44%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxDepth(String s) {
+        int cnt=0; int max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') cnt++;
+            if(s.charAt(i)==')') cnt--;
+            max = Math.max(max,cnt);
+        }
+        return max;
+    }
+}
