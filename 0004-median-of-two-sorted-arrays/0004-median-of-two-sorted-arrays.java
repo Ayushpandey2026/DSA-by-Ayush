e5d727aa0983133a8b79aeaaca3a430d2e26cc47
Ayush Pandey <ayushpandey02003@gmail.com>
2025-09-07 15:17:33 +0530

    Time: 1 ms (100%), Space: 46.3 MB (27.02%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,22 +1,33 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-         int m=nums1.length;
-        int n=nums2.length;
-        int[] arr=new int[m+n];
-        int i=0,j=0,k=0;
-        int val=0;
-        while(i<m && j<n){
-                if(nums1[i]<nums2[j]){
-                    arr[k++]=nums1[i++];
-                }
-               else  arr[k++]=nums2[j++];
-        }
-         while(i<m) arr[k++]=nums1[i++];
-         while(j<n) arr[k++]=nums2[j++];
-         int l = arr.length;
-         if(l%2!=0) return (double)arr[l/2];
-         else {
-            return (double)(arr[l/2] + arr[l/2-1])/2;
-         }
+    public double findMedianSortedArrays(int[] A, int[] B) {
+    if (A.length > B.length) {
+        return findMedianSortedArrays(B, A);
     }
+
+    int m = A.length, n = B.length;
+    int low = 0, high = m, halfLen = (m + n + 1) / 2;
+    while (low <= high) {
+        int i = (low + high) / 2;
+        int j = halfLen - i;
+
+        int Aleft = (i == 0) ? Integer.MIN_VALUE : A[i - 1];
+        int Aright = (i == m) ? Integer.MAX_VALUE : A[i];
+        int Bleft = (j == 0) ? Integer.MIN_VALUE : B[j - 1];
+        int Bright = (j == n) ? Integer.MAX_VALUE : B[j];
+
+        if (Aleft <= Bright && Bleft <= Aright) {
+            if ((m + n) % 2 == 1) {
+                return Math.max(Aleft, Bleft);
+            } else {
+                return ( (double)Math.max(Aleft, Bleft) + Math.min(Aright, Bright) ) / 2;
+            }
+        } else if (Aleft > Bright) {
+            high = i - 1;
+        } else {
+            low = i + 1;
+        }
+    }
+    throw new IllegalArgumentException("No median found");
+}
+
 }
