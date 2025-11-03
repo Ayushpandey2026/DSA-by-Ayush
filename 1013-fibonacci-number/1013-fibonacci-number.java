ef0e88e01c74790218b6e295cc4f949b77425d71
Ayush Pandey <ayushpandey02003@gmail.com>
2025-11-03 15:27:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (7.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
-    public int fibo(int n, int[] dp){
+    static int[] dp;
+    public int fibo(int n){
         if(n<=1) return n;
         if(dp[n]!=0) return dp[n];
-        int ans  = fibo(n-1,dp) + fibo(n-2,dp);
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n]=ans;
         return ans;
     }
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        return fibo(n,dp);
+        dp = new int[n+1];
+       return fibo(n);
     }
 }
