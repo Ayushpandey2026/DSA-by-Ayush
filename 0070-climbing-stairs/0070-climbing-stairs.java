ea770475f2d8bf5b12f45dddedcad697511dd256
Ayush Pandey <ayushpandey02003@gmail.com>
2025-11-16 22:28:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (6.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climb(int n, int[] dp){
-         if(n<=1) return 1;
+         if(n<=3) return n;
          if(dp[n]!=-1) return dp[n];
          return dp[n] = climb(n-1,dp) + climb(n-2,dp); 
     }
