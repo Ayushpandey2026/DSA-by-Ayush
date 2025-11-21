63ddf6140d1fd7439db1e6c1640a8169f76d6412
Ayush Pandey <ayushpandey02003@gmail.com>
2025-11-21 16:06:15 +0530

    Time: 26 ms (75.76%), Space: 44.6 MB (29.49%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isperfect(int n){
+        int sqrt = (int)(Math.sqrt(n));
+        return (sqrt*sqrt)==n;
+    }
+    public int cntSquares(int n, int[] dp) {
+        if(isperfect(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i<=n/2;i++){
+            int cnt= cntSquares(i,dp) + cntSquares(n-i,dp);
+            min = Math.min(min,cnt);
+        }
+        return dp[n]= min;
+    }
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        for(int i=1;i<=n;i++){
+            if(isperfect(i)) dp[i]= 1;
+            else{
+                int min = Integer.MAX_VALUE;
+                for(int j=1;j*j<=i;j++){
+                    int cnt = dp[j*j] + dp[i-j*j];
+                    min=  Math.min(min,cnt);
+                }
+                dp[i]=min;
+            }
+        }
+      return dp[n];
+        
+    }
+}
