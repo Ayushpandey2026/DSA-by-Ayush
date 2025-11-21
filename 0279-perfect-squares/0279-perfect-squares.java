76b58cc9dec385003641b897cb3a868d4a5d6566
Ayush Pandey <ayushpandey02003@gmail.com>
2025-11-21 16:47:46 +0530

    Time: 194 ms (9.6%), Space: 44.6 MB (29.49%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -7,8 +7,8 @@ class Solution {
         if(isperfect(n)) return 1;
         if(dp[n]!=-1) return dp[n];
         int min = Integer.MAX_VALUE;
-        for(int i=1;i<=n/2;i++){
-            int cnt= cntSquares(i,dp) + cntSquares(n-i,dp);
+        for(int i=1;i*i<=n;i++){
+            int cnt= cntSquares(i*i,dp) + cntSquares(n-i*i,dp);
             min = Math.min(min,cnt);
         }
         return dp[n]= min;
@@ -16,18 +16,19 @@ class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        for(int i=1;i<=n;i++){
-            if(isperfect(i)) dp[i]= 1;
-            else{
-                int min = Integer.MAX_VALUE;
-                for(int j=1;j*j<=i;j++){
-                    int cnt = dp[j*j] + dp[i-j*j];
-                    min=  Math.min(min,cnt);
-                }
-                dp[i]=min;
-            }
+        // for(int i=1;i<=n;i++){
+        //     if(isperfect(i)) dp[i]= 1;
+        //     else{
+        //         int min = Integer.MAX_VALUE;
+        //         for(int j=1;j*j<=i;j++){
+        //             int cnt = dp[j*j] + dp[i-j*j];
+        //             min=  Math.min(min,cnt);
+        //         }
+        //         dp[i]=min;
+        //     }
+        return cntSquares(n,dp);
         }
-      return dp[n];
+    //   return dp[n];
         
-    }
+    
 }
