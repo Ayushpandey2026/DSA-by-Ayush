cbff15e3e491c210a12259c90156bf9c66e1ebb1
Ayush Pandey <ayushpandey02003@gmail.com>
2025-11-23 18:32:21 +0530

    Time: 7 ms (67.13%), Space: 47.2 MB (5.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,23 @@
 class Solution {
-    public int ways(int i, int[] nums, int target,int sum){
+    static int res =0;
+    public int ways(int i, int[] nums, int target,int sum,int[][] dp){
          if(i==nums.length){
-        if(sum==target) {
-            return 1;
-        }
+        if(sum==target) return 1;
         else return 0;
          }
-
-     int add =  ways(i+1,nums,target,sum - nums[i]);
-     int sub  =  ways(i+1,nums,target,sum + nums[i]);
-     return add+sub;
+     if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+     int sub =  ways(i+1,nums,target,sum - nums[i],dp);
+     int add  =  ways(i+1,nums,target,sum + nums[i],dp);
+     return dp[i][res+sum]= add+sub;
 
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(0,nums,target,0);
+        res =0;
+        for(int i=0;i<nums.length;i++) res+=nums[i];
+        int[][] dp=new int[nums.length][2*res+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return ways(0,nums,target,0,dp);
     }
 }
