fbb2725a8b2f4f17a52640365e7b5977b3af5ea5
Ayush Pandey <ayushpandey02003@gmail.com>
2025-12-01 18:33:56 +0530

    Time: 67 ms (10.43%), Space: 108.4 MB (5.04%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -22,16 +22,14 @@ class Solution {
 
         Arrays.sort(arr);
 
-        List<Integer> temp = new ArrayList<>();
-        for (Envelop e : arr) {
-            int h = e.h;
-            int idx =Collections.binarySearch(temp, h);
-
-            if (idx <0) idx = -(idx + 1);
-
-            if (idx == temp.size()) temp.add(h);
-            else temp.set(idx, h);
+        List<Integer> list = new ArrayList<>();
+        for(Envelop e: arr){
+            int h=e.h;
+            int idx = Collections.binarySearch(list,h);
+            if(idx<0) idx = -(idx+1);
+            if(idx==list.size()) list.add(h);
+            else list.set(idx,h);
         }
-        return temp.size();
+        return list.size();
     }
 }
