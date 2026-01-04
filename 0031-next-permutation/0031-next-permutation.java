21079f8e84f9874146931598882f3028fa7fccc1
Ayush Pandey <ayushpandey02003@gmail.com>
2026-01-04 15:25:51 +0530

    Time: 0 ms (100%), Space: 44.6 MB (68.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;  int j=0,i=n-2; int cnt=0;
-        for(i=n-2;i>=0;i--){
-            if(nums[i+1]>nums[i]){
-                cnt++;
-                for(j=n-1;j>i;j--){
-                    if(nums[i]<nums[j]){
-                       break;
-                    }
-                }
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
-                int a = i+1, b=n-1;
-                while(a<b){
-                    int t = nums[a];
-                    nums[a] = nums[b];
-                    nums[b] = t;
-                    a++;
-                    b--;
-                }
-                return;
-            }
-        } 
-        int a = 0, b=n-1;
-        if(cnt==0) {
-                while(b<n &&  a<=b){
-                    int t = nums[a];
-                    nums[a] = nums[b];
-                    nums[b] = t;
-                    a++;
-                    b--;
-                }
+        int n=nums.length;
+        int i=n-1,j=n-2;
+        while(j>=0 && nums[j]>=nums[j+1]){
+            j--;
         }
+
+        if(j>=0){
+        while(i>=j){
+            if(nums[i]>nums[j]) break;
+            i--;
+        }
+             int temp=nums[j];
+        nums[j]=nums[i];
+        nums[i]=temp;
+        }
+       
+
+        int left = j + 1;
+       int right = n - 1;
+
+        while (left < right) {
+       int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+       }
     }
 }
