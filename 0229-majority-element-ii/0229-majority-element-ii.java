3f282245ddc343e39c351d700f7500397ac52ae3
Ayush Pandey <ayushpandey02003@gmail.com>
2026-01-04 07:48:19 +0530

    Time: 2 ms (99.77%), Space: 52.9 MB (20.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
+        int candidate1 =0; int cnt1=0;
+        int candidate2=0; int cnt2=0;
         for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+           if(num==candidate1){
+                cnt1++;
+            }
+            else if(num==candidate2){
+                cnt2++;
+            }
+           else if(cnt1==0) {
+                candidate1=num;
+                cnt1=1;
+            }
+            else if(cnt2==0) {
+                candidate2=num;
+                cnt2=1;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int num : nums){
+            if(num==candidate1) cnt1++;
+            else if(num==candidate2) cnt2++;
         }
 
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if(value>nums.length/3) list.add(key);
-        }
+        if(cnt1>nums.length/3) list.add(candidate1);
+        if(cnt2>nums.length/3) list.add(candidate2);
+
+       
         return list;
     }
 }
