33c4b7c7887d9fafcf67cfa7fed015e13b5b40f8
Ayush Pandey <ayushpandey02003@gmail.com>
2026-01-12 13:51:22 +0530

    Time: 0 ms (100%), Space: 48.2 MB (37.51%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    private double pow(double x,long n){
+       if(n==0) return 1.00;
+       double half =pow(x,n/2);
+       if(n%2==0) return half*half;
+       else return half*half*x;
+    }
+    public double myPow(double x, int n) {
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return pow(x,N);
+    }
+}
