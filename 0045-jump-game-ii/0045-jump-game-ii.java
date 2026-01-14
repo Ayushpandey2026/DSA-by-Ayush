cadf5d8e5f253058c01664b721b03bca66a79b7f
Ayush Pandey <ayushpandey02003@gmail.com>
2026-01-14 16:53:06 +0530

    Time: 1 ms (99.4%), Space: 47.5 MB (11.6%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,25 +1,17 @@
-public class Solution {
+class Solution {
     public int jump(int[] nums) {
-        int jumps = 0;          // Number of jumps made
-        int currentEnd = 0;     // End of the range for the current jump
-        int farthest = 0;       // Farthest index reachable in the next jump
+        int jumps = 0;
+        int currEnd = 0;
+        int farthest = 0;
 
         for (int i = 0; i < nums.length - 1; i++) {
-            // Update the farthest point we can reach from current position
             farthest = Math.max(farthest, i + nums[i]);
 
-            // When we reach the end of the current jump range
-            if (i == currentEnd) {
-                jumps++;             // We must jump here
-                currentEnd = farthest; // Set new range end
-
-                // Optional: early exit if we can already reach the end
-                if (currentEnd >= nums.length - 1) {
-                    break;
-                }
+            if (i == currEnd) {
+                jumps++;
+                currEnd = farthest;
             }
         }
-
         return jumps;
     }
 }
