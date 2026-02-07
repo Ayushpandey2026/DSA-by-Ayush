b5e26a3d80b92f529b8181f86fa5d3f50b71c51d
Ayush Pandey <ayushpandey02003@gmail.com>
2026-02-07 20:31:02 +0530

    Time: 12 ms (92.76%), Space: 74.1 MB (35.71%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -2,19 +2,16 @@ class Solution {
     public long maxRunTime(int n, int[] batteries) {
 
         long total = 0;
-        for (int b : batteries) total += b;
+        for(int b : batteries) total += b;
 
         long low = 0;
         long high = total / n;   
 
-        while (low < high) {
+        while(low < high) {
             long mid = (low + high + 1) / 2;
 
-            if (canRun(mid, n, batteries)) {
-                low = mid;
-            } else {
-                high = mid - 1;
-            }
+            if(canRun(mid, n, batteries)) low = mid; 
+            else high = mid - 1;
         }
 
         return low;
