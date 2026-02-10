9520f0468dbdb283f9b382c9014d4b1a8a787c4c
Ayush Pandey <ayushpandey02003@gmail.com>
2026-02-10 09:07:30 +0530

    Time: 39 ms (40.3%), Space: 47.1 MB (70.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -6,20 +6,20 @@ class Solution {
 
         dp[0][0] = true;
 
-        for (int j = 1; j <= m; j++) {
+        for(int j = 1; j <= m; j++){
             if (p.charAt(j - 1) == '*') {
                 dp[0][j] = dp[0][j - 1];
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
                 char pChar = p.charAt(j - 1);
-                if (pChar == '?' || pChar == s.charAt(i - 1)) {
+                if(pChar == '?' || pChar == s.charAt(i - 1))
                     dp[i][j] = dp[i - 1][j - 1];
-                } else if (pChar == '*') {
+                    else if (pChar == '*') 
                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                }
+                
             }
         }
 
