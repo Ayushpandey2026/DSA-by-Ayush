cde8e5f56bb4788dbce87f77513be39d86fa3ab3
Ayush Pandey <ayushpandey02003@gmail.com>
2026-04-04 05:26:30 +0530

    Time: 4 ms (74.36%), Space: 45 MB (71.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,13 +4,12 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>(); 
         Stack<Integer> st = new Stack<>();
 
-        for(int num : nums2){
-            while(!st.isEmpty() && st.peek()<num){
-                map.put(st.pop(),num);
-            }
-            st.push(num);
+       for(int num :nums2){
+        while(!st.isEmpty() && st.peek()<num){
+            map.put(st.pop(),num);
         }
-
+        st.push(num);
+       }
         for(int i=0;i<nums1.length;i++){
             arr[i] = map.getOrDefault(nums1[i],-1);
         }
