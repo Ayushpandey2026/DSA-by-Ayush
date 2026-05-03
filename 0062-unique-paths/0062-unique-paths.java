76971599f374b024ebeeb79c7f149186290b1a20
Ayush Pandey <ayushpandey02003@gmail.com>
2026-05-03 09:48:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,5 @@
 class Solution {
     public int paths(int m, int n, int i, int j,int[][] dp){
-        // if(i>=m || j>=n) return 0;
         if(i==m-1 || j==n-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
       int rw = paths(m,n,i+1,j,dp);
