0ca2cb987fd33f5e7aa36657dd15791ff5bb5771
Ayush Pandey <ayushpandey02003@gmail.com>
2026-09-05 07:34:14 +0530

    Time: 31 ms (19.32%), Space: 47.7 MB (8.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,25 @@
 class Solution {
-    public int count(int[] coins,int i, int amount, int[][] dp){
+
+    public int makeup(int i, int[] coins, int amount,int[][] dp){
         if(amount==0) return 0;
-        if(i==coins.length || amount < 0) return (int)1e9;
+        if(i==coins.length || amount<0) return (int) 1e9;
 
         if(dp[i][amount]!=-1) return dp[i][amount];
-        int skip =count(coins,i+1,amount,dp);
-        int pick =1 + count(coins,i,amount-coins[i],dp);
 
-       return  dp[i][amount] =Math.min(skip,pick);
+        int take= 1 + makeup(i,coins,amount-coins[i],dp);
+        int skip= makeup(i+1, coins,amount,dp);
+
+        return dp[i][amount]=Math.min(take,skip);
     }
+
     public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+
         int[][] dp =new int[coins.length][amount+1];
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-       int ans= count(coins,0,amount,dp);
-       return ans>=1e9 ? -1 : ans;
+        int ans= makeup(0,coins,amount,dp);
+      return ans>=1e9 ? -1 : ans;
     }
 }
